e03a29ad45279d25effd0ec9eed0bae87266b565
Julien Elbaz <julien.elbaz@web-education.net>
2016-08-10 16:50:49 +0200

    [Evo] Updated notifications urls, now pointing to the right domain

--- src/main/java/net/atos/entng/mindmap/controllers/MindmapController.java
+++ src/main/java/net/atos/entng/mindmap/controllers/MindmapController.java
@@ -163,10 +163,10 @@ public class MindmapController extends MongoDbControllerHelper {
                     }
 
                     JsonObject params = new JsonObject();
-                    params.putString("uri", container.config().getString("host", "http://localhost:8090") +
+                    params.putString("uri", getScheme(request) + "://" + getHost(request) +
                     		"/userbook/annuaire#" + user.getUserId() + "#" + user.getType());
                     params.putString("username", user.getUsername());
-                    params.putString("mindmapUri", container.config().getString("host", "http://localhost:8090") +
+                    params.putString("mindmapUri", getScheme(request) + "://" + getHost(request) +
                     		"/mindmap#/view/" + id);
                     params.putString("resourceUri", params.getString("mindmapUri"));
 
