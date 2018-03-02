c7311aff9ce44aefec168b413a0999734555ac98
jrob <jeremy.robertou@gmail.com>
2018-03-02 17:51:09 +0100

    Wip typescript migration

---
+++ src/main/resources/public/ts/model/Mindmaps.ts
@@ -0,0 +1,41 @@
+import { _ } from 'entcore';
+import { Selection } from 'entcore-toolkit';
+import { Mindmap } from './index';
+import http from 'axios';
+
+export class Mindmaps extends Selection<Mindmap> {
+    all: Mindmap[];
+
+    constructor() {
+        super([]);
+    };
+
+    sync (callback) {
+        http.get('/mindmap/list/all').then((mindmaps) => {
+
+            this.all = [];
+            _.forEach(mindmaps.data, (mindmap) => {
+                this.all.push(new Mindmap(mindmap));
+            });
+
+            if(typeof callback === 'function'){
+                callback();
+            }
+        });
+    };
+
+    remove (item) {
+        this.all = _.remove(this.all, function(n) {
+            return n === item;
+        });
+    };
+
+    forEach (cb) {
+        return _.forEach(this.all, cb);
+    };
+
+    selection () {
+        return this.selected;
+    }
+};
+
