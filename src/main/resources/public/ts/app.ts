fd1d2544fb8506179f8616e0e28a28bda9d1466a
jrob <jeremy.robertou@gmail.com>
2018-03-02 17:51:09 +0100

    Wip typescript migration

---
+++ src/main/resources/public/ts/app.ts
@@ -0,0 +1,41 @@
+import { ng, model, routes } from 'entcore';
+
+import { Mindmap } from './model';
+import { Mindmaps } from './model';
+import * as controllers from './controllers';
+import * as directives from './directives';
+
+declare let module: any;
+
+for (let controller in controllers) {
+    ng.controllers.push(controllers[controller]);
+}
+
+for (let directive in directives) {
+    ng.directives.push(directives[directive]);
+}
+
+
+/**
+ * Allows to create a model and load the list of mindmaps from the backend.
+ */
+model.build = function() {
+
+    this.mindmaps = new Mindmaps();
+    this.mindmaps.sync();
+};
+
+
+/**
+ * Allows to define routes of collaborative walls application.
+ */
+routes.define(($routeProvider) => {
+    $routeProvider
+        .when('/view/:mindmapId', {
+            action: 'viewMindmap'
+        })
+        .otherwise({
+            action: 'listMindmap'
+        });
+
+});
---
+++ src/main/resources/public/ts/model/index.ts
@@ -0,0 +1,2 @@
+export * from './Mindmaps';
+export * from './Mindmap';
