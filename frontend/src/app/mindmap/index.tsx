46b49f9217fcd80c23df9ab711ce72263119651b
Lucien Devars du Mayne <lucien.devarsdumayne@opendigitaleducation.com>
2023-07-12 17:48:59 +0200

    feat: #WB2-1011, add AppHeader on mindmap

--- frontend/src/app/mindmap/index.tsx
+++ frontend/src/app/mindmap/index.tsx
@@ -4,6 +4,7 @@ import { useOdeClient } from "@ode-react-ui/core";
 import { ID } from "ode-ts-client";
 import { LoaderFunctionArgs, useLoaderData, useParams } from "react-router-dom";
 
+// eslint-disable-next-line import/order
 import {
   mapInfo,
   options,
@@ -11,6 +12,11 @@ import {
 } from "~/features/mindmap/configuration";
 
 import "~/styles/index.css";
+
+// eslint-disable-next-line import/order
+import { AppHeader } from "@ode-react-ui/components";
+// eslint-disable-next-line import/order
+import { AppAction } from "../components/AppAction";
 
 export interface MindmapProps {
   created: Date;
@@ -33,11 +39,17 @@ export async function mapLoader({ params }: LoaderFunctionArgs) {
 
 export const Mindmap = () => {
   const data = useLoaderData() as MindmapProps;
+  const { currentApp } = useOdeClient();
   const { currentLanguage } = useOdeClient();
   const params = useParams();
 
   return data?.map ? (
     <>
+      <AppHeader
+        app={currentApp}
+        resourceName={data?.name}
+        actions={<AppAction />}
+      />
       <div className="mindplot-div-container">
         <Editor
           mapInfo={mapInfo(data?.name)}
